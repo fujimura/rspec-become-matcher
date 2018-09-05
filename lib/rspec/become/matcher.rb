90755dcd903acf73605aaa1a2be21d6e7e670bf7
Daisuke Fujimura <me@fujimuradaisuke.com>
2018-09-05 19:08:12 +0900

    Not used

--- lib/rspec/become/matcher.rb
+++
@@ -1,9 +0,0 @@
-require "rspec/become/matcher/version"
-
-module Rspec
-  module Become
-    module Matcher
-      # Your code goes here...
-    end
-  end
-end
--- lib/rspec/become/matcher/version.rb
+++
@@ -1,7 +0,0 @@
-module Rspec
-  module Become
-    module Matcher
-      VERSION = "0.1.0"
-    end
-  end
-end
